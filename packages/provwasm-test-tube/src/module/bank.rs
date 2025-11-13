a00931935232780553b067fdcd18030c36bc6263
kwt <4344285+kwtalley@users.noreply.github.com>
2025-11-13 11:58:41 -0600

    update cosmos modules endpoints

--- packages/provwasm-test-tube/src/module/bank.rs
+++ packages/provwasm-test-tube/src/module/bank.rs
@@ -1,8 +1,14 @@
 use provwasm_std::types::cosmos::bank::v1beta1::{
     MsgMultiSend, MsgMultiSendResponse, MsgSend, MsgSendResponse, QueryAllBalancesRequest,
-    QueryAllBalancesResponse, QueryBalanceRequest, QueryBalanceResponse, QueryDenomMetadataRequest,
-    QueryDenomMetadataResponse, QueryParamsRequest, QueryParamsResponse, QuerySupplyOfRequest,
-    QuerySupplyOfResponse,
+    QueryAllBalancesResponse, QueryBalanceRequest, QueryBalanceResponse,
+    QueryDenomMetadataByQueryStringRequest, QueryDenomMetadataByQueryStringResponse,
+    QueryDenomMetadataRequest, QueryDenomMetadataResponse, QueryDenomOwnersByQueryRequest,
+    QueryDenomOwnersByQueryResponse, QueryDenomOwnersRequest, QueryDenomOwnersResponse,
+    QueryDenomsMetadataRequest, QueryDenomsMetadataResponse, QueryParamsRequest,
+    QueryParamsResponse, QuerySendEnabledRequest, QuerySendEnabledResponse,
+    QuerySpendableBalanceByDenomRequest, QuerySpendableBalanceByDenomResponse,
+    QuerySpendableBalancesRequest, QuerySpendableBalancesResponse, QuerySupplyOfRequest,
+    QuerySupplyOfResponse, QueryTotalSupplyRequest, QueryTotalSupplyResponse,
 };
 
 use test_tube_prov::module::Module;
@@ -39,14 +45,46 @@ where
     }
 
     fn_query! {
-        pub query_denom_metadata ["/cosmos.bank.v1beta1.Query/DenomMetadata"]: QueryDenomMetadataRequest => QueryDenomMetadataResponse
+        pub query_spendable_balances ["/cosmos.bank.v1beta1.Query/SpendableBalances"]: QuerySpendableBalancesRequest => QuerySpendableBalancesResponse
     }
 
     fn_query! {
-        pub query_params ["/cosmos.bank.v1beta1.Query/Params"]: QueryParamsRequest => QueryParamsResponse
+        pub query_spendable_balances_by_denom ["/cosmos.bank.v1beta1.Query/SpendableBalanceByDenom"]: QuerySpendableBalanceByDenomRequest => QuerySpendableBalanceByDenomResponse
+    }
+
+    fn_query! {
+        pub query_total_supply ["/cosmos.bank.v1beta1.Query/TotalSupply"]: QueryTotalSupplyRequest => QueryTotalSupplyResponse
     }
 
     fn_query! {
         pub query_supply_of ["/cosmos.bank.v1beta1.Query/SupplyOf"]: QuerySupplyOfRequest => QuerySupplyOfResponse
+    }
+
+    fn_query! {
+        pub query_denom_metadata ["/cosmos.bank.v1beta1.Query/DenomMetadata"]: QueryDenomMetadataRequest => QueryDenomMetadataResponse
+    }
+
+    fn_query! {
+        pub query_denom_metadata_by_query_string ["/cosmos.bank.v1beta1.Query/DenomMetadataByQueryString"]: QueryDenomMetadataByQueryStringRequest => QueryDenomMetadataByQueryStringResponse
+    }
+
+    fn_query! {
+        pub query_denoms_metadata ["/cosmos.bank.v1beta1.Query/DenomsMetadata"]: QueryDenomsMetadataRequest => QueryDenomsMetadataResponse
+    }
+
+    fn_query! {
+        pub query_denom_owners ["/cosmos.bank.v1beta1.Query/DenomOwners"]: QueryDenomOwnersRequest => QueryDenomOwnersResponse
+    }
+
+    fn_query! {
+        pub query_denom_owners_by_query ["/cosmos.bank.v1beta1.Query/DenomOwnersByQuery"]: QueryDenomOwnersByQueryRequest => QueryDenomOwnersByQueryResponse
+    }
+
+    fn_query! {
+        pub query_send_enabled ["/cosmos.bank.v1beta1.Query/SendEnabled"]: QuerySendEnabledRequest => QuerySendEnabledResponse
+    }
+
+    fn_query! {
+        pub query_params ["/cosmos.bank.v1beta1.Query/Params"]: QueryParamsRequest => QueryParamsResponse
     }
 }
--- packages/provwasm-test-tube/src/module/gov.rs
+++ packages/provwasm-test-tube/src/module/gov.rs
@@ -2,8 +2,21 @@ use test_tube_prov::{fn_execute, fn_query, Module, Runner};
 
 use provwasm_std::types::cosmos::gov::v1beta1::{
     MsgSubmitProposal, MsgSubmitProposalResponse, MsgVote, MsgVoteResponse, QueryDepositRequest,
-    QueryDepositResponse, QueryParamsRequest, QueryParamsResponse, QueryVoteRequest,
-    QueryVoteResponse,
+    QueryDepositResponse, QueryDepositsRequest, QueryDepositsResponse, QueryParamsRequest,
+    QueryParamsResponse, QueryProposalRequest, QueryProposalResponse, QueryProposalsRequest,
+    QueryProposalsResponse, QueryTallyResultRequest, QueryTallyResultResponse, QueryVoteRequest,
+    QueryVoteResponse, QueryVotesRequest, QueryVotesResponse,
+};
+use provwasm_std::types::cosmos::gov::v1::{
+    QueryConstitutionRequest, QueryConstitutionResponse, QueryDepositRequest as QueryDepositRequestV1,
+    QueryDepositResponse as QueryDepositResponseV1, QueryDepositsRequest as QueryDepositsRequestV1,
+    QueryDepositsResponse as QueryDepositsResponseV1, QueryParamsRequest as QueryParamsRequestV1,
+    QueryParamsResponse as QueryParamsResponseV1, QueryProposalRequest as QueryProposalRequestV1,
+    QueryProposalResponse as QueryProposalResponseV1, QueryProposalsRequest as QueryProposalsRequestV1,
+    QueryProposalsResponse as QueryProposalsResponseV1, QueryTallyResultRequest as QueryTallyResultRequestV1,
+    QueryTallyResultResponse as QueryTallyResultResponseV1, QueryVoteRequest as QueryVoteRequestV1,
+    QueryVoteResponse as QueryVoteResponseV1, QueryVotesRequest as QueryVotesRequestV1,
+    QueryVotesResponse as QueryVotesResponseV1,
 };
 
 pub struct Gov<'a, R: Runner<'a>> {
@@ -38,5 +51,61 @@ where
 
     fn_query! {
         pub query_deposit ["/cosmos.gov.v1beta1.Query/Deposit"]: QueryDepositRequest => QueryDepositResponse
+    }
+
+    fn_query! {
+        pub query_proposal ["/cosmos.gov.v1beta1.Query/Proposal"]: QueryProposalRequest => QueryProposalResponse
+    }
+
+    fn_query! {
+        pub query_proposals ["/cosmos.gov.v1beta1.Query/Proposals"]: QueryProposalsRequest => QueryProposalsResponse
+    }
+
+    fn_query! {
+        pub query_deposits ["/cosmos.gov.v1beta1.Query/Deposits"]: QueryDepositsRequest => QueryDepositsResponse
+    }
+
+    fn_query! {
+        pub query_tally_result ["/cosmos.gov.v1beta1.Query/TallyResult"]: QueryTallyResultRequest => QueryTallyResultResponse
+    }
+
+    fn_query! {
+        pub query_votes ["/cosmos.gov.v1beta1.Query/Votes"]: QueryVotesRequest => QueryVotesResponse
+    }
+
+    fn_query! {
+        pub query_constitution ["/cosmos.gov.v1.Query/Constitution"]: QueryConstitutionRequest => QueryConstitutionResponse
+    }
+
+    fn_query! {
+        pub query_proposal_v1 ["/cosmos.gov.v1.Query/Proposal"]: QueryProposalRequestV1 => QueryProposalResponseV1
+    }
+
+    fn_query! {
+        pub query_proposals_v1 ["/cosmos.gov.v1.Query/Proposals"]: QueryProposalsRequestV1 => QueryProposalsResponseV1
+    }
+
+    fn_query! {
+        pub query_vote_v1 ["/cosmos.gov.v1.Query/Vote"]: QueryVoteRequestV1 => QueryVoteResponseV1
+    }
+
+    fn_query! {
+        pub query_votes_v1 ["/cosmos.gov.v1.Query/Votes"]: QueryVotesRequestV1 => QueryVotesResponseV1
+    }
+
+    fn_query! {
+        pub query_params_v1 ["/cosmos.gov.v1.Query/Params"]: QueryParamsRequestV1 => QueryParamsResponseV1
+    }
+
+    fn_query! {
+        pub query_deposit_v1 ["/cosmos.gov.v1.Query/Deposit"]: QueryDepositRequestV1 => QueryDepositResponseV1
+    }
+
+    fn_query! {
+        pub query_deposits_v1 ["/cosmos.gov.v1.Query/Deposits"]: QueryDepositsRequestV1 => QueryDepositsResponseV1
+    }
+
+    fn_query! {
+        pub query_tally_result_v1 ["/cosmos.gov.v1.Query/TallyResult"]: QueryTallyResultRequestV1 => QueryTallyResultResponseV1
     }
 }
