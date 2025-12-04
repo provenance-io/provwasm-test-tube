4aefce9a04179c0f1fa1cdf3ff65e97c5b5b7c15
kwt <4344285+kwtalley@users.noreply.github.com>
2025-12-03 23:40:51 -0600

    missed chain-id swap

--- packages/provwasm-test-tube/src/runner/app.rs
+++ packages/provwasm-test-tube/src/runner/app.rs
@@ -6,7 +6,7 @@ use test_tube_prov::BaseApp;
 
 const FEE_DENOM: &str = "nhash";
 const PROV_ADDRESS_PREFIX: &str = "tp";
-const CHAIN_ID: &str = "testnet";
+const CHAIN_ID: &str = "testchain";
 
 #[derive(Debug, PartialEq)]
 pub struct ProvwasmTestApp {
